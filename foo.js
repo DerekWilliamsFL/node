e3f181f6caf310a5230a7ba53d17013b05058433
Derek Williams <derekwilliamsfl@gmail.com>
2015-12-03 12:54:07 -0500

    League subreddit scraper

---
+++ foo.js
@@ -0,0 +1,37 @@
+var cheerio = require('cheerio');
+var request = require('request');
+var titles    = [];
+var http    = require('http');
+
+request('http://www.reddit.com/r/leagueoflegends', function(err, resp, body) {
+	if(!err && resp.statusCode == 200) {
+		var $ = cheerio.load(body);
+
+		for(i = 0; i < $('a.title').length; i++) {
+		//$('a.title').each(function(){
+			//var url = $('a.title');
+			//urls.push(url);
+
+			var title = $('a.title')['' + i + ''].children[0].data;
+			//console.log(title);
+			titles.push(title);
+			if (title.indexOf('Megathread') !== -1) {
+				console.log("There's a megathread!!")
+			}
+
+		//});
+		}
+		console.log(titles);
+	}
+
+	else {
+		console.log("Error");
+	}
+});
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(titles.toString().split(',').join("\n").toString());
+}).listen(3618, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:3618/');
