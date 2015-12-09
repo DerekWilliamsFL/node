facbbc9663d415ecec78338054b64e085df52037
Derek Williams <derekwilliamsfl@gmail.com>
2015-12-09 16:46:04 -0500

    Can read OP's message, need top 10 comments.

--- foo.js
+++ foo.js
@@ -1,42 +1,71 @@
 var cheerio = require('cheerio');
 var request = require('request');
 var titles  = [];
+var sub = {};
 var http    = require('http');
 var prompt  = require('prompt');
+/*var program = require('commander');
+program.option('-1', '--first');
+program.parse(process.argv);
+if (program.first) console.log('First thread');*/
 
-var sub = {};
 
 //Future Promise
 prompt.start();
- 
 
-prompt.addProperties(sub, ['subreddit'], function (err) {
+	prompt.addProperties(sub, ['subreddit'], function (err) {
 
-	console.log('Updating..')
+		console.log('Updating..')
 
-	request('http://www.reddit.com/r/' + sub.subreddit + '', function(err, resp, body) {
-		if(!err && resp.statusCode == 200) {
-			var $ = cheerio.load(body);
-			var titleLength = $('a.title').length;
-			for(i = 0; i < titleLength; i++) {
-				var title = $('a.title')['' + i + ''].children[0].data;
-				titles.push(title);
-				if (title.indexOf('Megathread') !== -1) {
-					console.log("There's a megathread!!")
+		request('http://www.reddit.com/r/' + sub.subreddit + '', function(err, resp, body) {
+			if(!err && resp.statusCode == 200) {
+				var $ = cheerio.load(body);
+				var titleLength = $('a.title').length;
+				for(i = 0; i < titleLength; i++) {
+					var title = $('a.title')['' + i + ''].children[0].data;
+					var link  = $('.first a[href]')['' + i + ''].attribs.href;
+					titles.push({
+						'thread' : i,
+						'title': title,
+						'link' : link
+					});
 				}
+				console.log(titles);
+				//Works!
+				//console.log(titles[2]);
 			}
-			console.log(titles);
-		}
-		else {
-			console.log("Error");
-		}
+			else {
+				console.log("Error");
+			}
+		});
+
+		prompt.get(['thread'], function (err, result) {
+			
+			var threadNo = result.thread
+			var url = titles[threadNo].link;
+			console.log(url)
+			console.log(threadNo)
+			request(url, function(err, resp, body) {
+			if(!err && resp.statusCode == 200) {
+				var $ = cheerio.load(body);
+				//Respond with the 10 highest parent-less comments
+				//console.log(url);
+
+				//OP's message
+				console.log($('.expando .md > p')['0'].children[0].data);
+
+				//Best Comments~
+			}
+				else {
+				console.log("Error");
+				}
+			});
+		});
 	});
-});
 
-
-http.createServer(function (req, res) {
+/*http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('[' + sub.subreddit + ']' + '\n' + '\n');
   res.end(titles.toString().split(',').join("\n").toString());
 }).listen(3618, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:3618/');
+console.log('Server running at http://127.0.0.1:3618/');*/
