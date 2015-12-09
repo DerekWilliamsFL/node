a44960660c3a946e5dd56319756d2ab19821da00
Derek Williams <derekwilliamsfl@gmail.com>
2015-12-09 13:04:52 -0500

    Added subreddit choice functionality

--- foo.js
+++ foo.js
@@ -1,37 +1,42 @@
 var cheerio = require('cheerio');
 var request = require('request');
-var titles    = [];
+var titles  = [];
 var http    = require('http');
+var prompt  = require('prompt');
 
-request('http://www.reddit.com/r/leagueoflegends', function(err, resp, body) {
-	if(!err && resp.statusCode == 200) {
-		var $ = cheerio.load(body);
-		var titleLength = $('a.title').length;
-		for(i = 0; i < titleLength; i++) {
-		//$('a.title').each(function(){
-			//var url = $('a.title');
-			//urls.push(url);
+var sub = {};
 
-			var title = $('a.title')['' + i + ''].children[0].data;
-			//console.log(title);
-			titles.push(title);
-			if (title.indexOf('Megathread') !== -1) {
-				console.log("There's a megathread!!")
+//Future Promise
+prompt.start();
+ 
+
+prompt.addProperties(sub, ['subreddit'], function (err) {
+
+	console.log('Updating..')
+
+	request('http://www.reddit.com/r/' + sub.subreddit + '', function(err, resp, body) {
+		if(!err && resp.statusCode == 200) {
+			var $ = cheerio.load(body);
+			var titleLength = $('a.title').length;
+			for(i = 0; i < titleLength; i++) {
+				var title = $('a.title')['' + i + ''].children[0].data;
+				titles.push(title);
+				if (title.indexOf('Megathread') !== -1) {
+					console.log("There's a megathread!!")
+				}
 			}
-
-		//});
+			console.log(titles);
 		}
-		console.log(titles);
-	}
-
-	else {
-		console.log("Error");
-	}
+		else {
+			console.log("Error");
+		}
+	});
 });
 
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('[' + sub.subreddit + ']' + '\n' + '\n');
   res.end(titles.toString().split(',').join("\n").toString());
 }).listen(3618, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:3618/');
